d2af7e5c52264144f2ce989ba39b2506b0cff704
Sam <samuelsantucci04@gmail.com>
2020-08-26 20:31:02 +0930

    Updated Physics
    Added RigidBody physics to player for movement and collisions

---
+++ Assets/Scripts/Player/Grounded.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {   
+            //if(transform.position.y > collision.contacts[0].point.y) // TODO???
+            {
+                Player.GetComponent<PlayerController>().isGrounded = true; //Changes the isGrounded bool in the player controller script to true if collided with the ground
+            }
+           
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerController>().isGrounded = false;
+             //Changes the isGrounded bool in the player controller script to false if not collided with the ground
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,156 +1,97 @@
 ﻿
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed;
-
     public float jumpHeight;
 
-    public Vector2 direction;
-
-    private Rigidbody2D rb;
+    
 
     private bool facingRight = true;
+    public bool isGrounded = false;
 
-    public Transform feet;
-   
-    public Vector3 velocity;
-    public float gravityMultiplier;
-
-
+    private Rigidbody2D rb;
     private Animator anim;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>(); //Gets the RigidBody2D from the player
         anim = GetComponent<Animator>(); //Gets the animator component
     }
 
     private void Update()
     {
+        Jump();
+
+       
+
+        Vector2 velocity = rb.velocity; 
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isJumping", false);
+
+        if (velocity.y < 0)
+        {
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isRunning", false);
+        }
+
+        velocity.x = 0;
+
+        if (Input.GetKey("a"))
+        {   
+            velocity.x -= movementSpeed;
+
+            if(Input.GetKey("a") && isGrounded)
+            {
+                anim.SetBool("isRunning", true);
+            }
+            
+        }
+
+        if (Input.GetKey("d"))
+        {
+            velocity.x += movementSpeed;
+            anim.SetBool("isRunning", true);
+        }
+
+        rb.velocity = velocity;
+
+
+        if (velocity.x < 0) //If the move input is less than zero, the player changes their direction to the left
+         {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            
+        }
+
+        else if (velocity.x > 0)  //If the move input is more than zero, the player changes their direction to the right
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
        
     }
 
     void FixedUpdate() //Use for physics
     {
-       
+        
 
+    }   
 
-        bool grounded = Physics2D.Raycast(feet.transform.position, Vector3.down, 0.1f);
+    void Jump()
+    {
+        
 
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //Gets the horizontal and vertical axis
-
-        if (!grounded) //If the object is not touching the ground, gravity is applied to the object
-
-
-
-            velocity += Physics.gravity * gravityMultiplier * Time.deltaTime;
-
-
-
-        else if (velocity.y < 0) //If the object is touching the ground, remove gravity
-
-
-            velocity = Vector3.zero;
-
-        transform.position += velocity * Time.deltaTime;
-
-
-        if (Input.GetKey("space") && grounded) //If the space key is pressed and the object is touching the ground, allow it to jump
-        {   
-
-            velocity.y = jumpHeight;
-        }
-
-       if (grounded)
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
-            anim.SetBool("isJumping", false);
+            rb.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
         }
-
-       else if (!grounded)
-        {
-            anim.SetBool("isRunning", false);
-            anim.SetBool("isJumping", true);
-
-
-        }
-
-
-
-       if (Input.GetKey("d") && Input.GetKey("a"))
-        {
-            velocity.x = 0;
-            anim.SetBool("isRunning", false);
-        }
-
-
-
-        else if (Input.GetKey("d")) //Gets the input of key d to move the object and sets it's velocity to it's movement speed
-        {
-            velocity.x = movementSpeed;
-
-            if (Input.GetKey("d") && grounded)
-            {
-                anim.SetBool("isRunning", true);
-            }
-
-            else
-            {
-                anim.SetBool("isRunning", false);
-            }
-
-        }
-
-
-       
-
-        else if (Input.GetKey("a"))  //Gets the input of key a to move the object and sets it's velocity to it's -movement speed
-
-        {
-            velocity.x = -movementSpeed;
-
-            if (Input.GetKey("a") && grounded)
-            {
-                anim.SetBool("isRunning", true);
-            }
-
-            else
-            {
-                anim.SetBool("isRunning", false);
-            }
-        }
-
-
-
-
-        else //If no other keys are pressed, velocity is zero and the object wont move
-        {
-           
-            velocity.x = 0f;
-            anim.SetBool("isRunning", false);
-         
-        }
-
-        float moveInput = Input.GetAxisRaw("Horizontal");
-
-
-
-
-
-
-        if (moveInput < 0) //If the move input is less than zero, the player changes their direction to the left
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-
-        else if (moveInput > 0)  //If the move input is more than zero, the player changes their direction to the right
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-
-      
     }
+     
+
+       
+      
+    
 
 
 
