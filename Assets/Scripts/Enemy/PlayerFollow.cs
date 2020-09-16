dc7d36d96c10f51e8db2feac7e5c3221c29d5965
Sam <samuelsantucci04@gmail.com>
2020-09-16 20:07:19 +0930

    Enemy +
    Added Prefab of Enemy phantom. Added code for following the player and line of site of the enemy. Added animation

---
+++ Assets/Scripts/Enemy/PlayerFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float lineOfSite;
+
+    private Transform target; //player enemy is chasing
+    
+    
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceFromPLayer = Vector2.Distance(target.position, transform.position);
+        if(distanceFromPLayer < lineOfSite)
+        {
+            if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        }
+     
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+    }
+}
