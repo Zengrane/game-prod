26f3274a8ee45fecea69ed7d15b207aee2e0689f
Sam <samuelsantucci04@gmail.com>
2020-09-23 20:10:25 +0930

    Enemy Projectiles
    Added enemy projectile shooting

--- Assets/Scripts/Enemy/PlayerFollow.cs
+++ Assets/Scripts/Enemy/PlayerFollow.cs
@@ -6,16 +6,21 @@ public class PlayerFollow : MonoBehaviour
 {
     public float speed;
     public float stoppingDistance;
+    public float shootingRange;
+    public float fireRate = 5f;
+    private float nextFireTime;
+    public GameObject enemyProj;
+    public GameObject enemyProjParent;
     public float lineOfSite;
 
     private Rigidbody2D rb2d;
 
     private Transform target; //player enemy is chasing
-    
-    
+
+
 
     // Start is called before the first frame update
-    void Start() 
+    void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         rb2d = GetComponent<Rigidbody2D>();
@@ -24,34 +29,47 @@ public class PlayerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         float distanceFromPLayer = Vector2.Distance(target.position, transform.position);
         if(distanceFromPLayer < lineOfSite)
         {
-            if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+            if (transform.position.x < target.position.x) //If the enemy position is less than x, the enemy moves right
             {
-                if(transform.position.x < target.position.x) //If the enemy position is less than x, the enemy moves right
-                {
-                    rb2d.velocity = new Vector2(speed, 0);
-                    transform.localScale = new Vector2(1.5f, 1.5f);
-
-                }
-
-                else if(transform.position.x > target.position.x) //If the enemy position is greater than x, the enemy moves left
-                {
-                    rb2d.velocity = new Vector2(-speed, 0);
-                    transform.localScale = new Vector2(-1.5f, 1.5f);
-                }
+                rb2d.velocity = new Vector2(speed, 0);
+                transform.localScale = new Vector2(1.5f, 1.5f);
 
             }
+
+            else if (transform.position.x > target.position.x) //If the enemy position is greater than x, the enemy moves right
+            {
+                rb2d.velocity = new Vector2(-speed, 0);
+                transform.localScale = new Vector2(-1.5f, 1.5f);
+
+            }
+
+
+        }
+
+        if (distanceFromPLayer <= shootingRange && nextFireTime < Time.time)
+        {
+            GameObject proj = Instantiate(enemyProj, enemyProjParent.transform.position, Quaternion.identity);
+
+       
+            proj.transform.up = target.position - transform.position;
+
+            nextFireTime = Time.time + fireRate;
         }
 
 
-     
     }
+
+
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
     }
 }
